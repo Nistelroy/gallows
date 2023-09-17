683bea625a10039d4baaf61e0851e7b05cff1d68
Nistel <duz3000@gmail.com>
2023-09-17 09:05:02 +0300

    полностью исправная версия

--- src/Gallows.java
+++ src/Gallows.java
@@ -10,7 +10,6 @@ public class Gallows {
             try {
                 counterChar = System.in.read(word);
             } catch (Exception ex) {
-                System.out.println(ex.toString());
                 break;
             }
             String strWord = new String(word, 0, counterChar).trim();
@@ -19,66 +18,10 @@ public class Gallows {
             } else if ("1".equalsIgnoreCase(strWord)){
                 Game go = new Game();
                 go.gameStart();
-                break;
+                System.out.println("Победа! Для начала новой игры введи:  1   Для выхода:  2  ");
             } else {
-                System.out.println("Ты набрал '" + strWord + "' а надо набрать   1   или   2   ");
+                System.out.println("Ты набрал '" + strWord + "' а надо набрать для начала игры:  1   Для выхода:  2  ");
             }
         }
     }
 }
-            /*
-            if ("выход".equalsIgnoreCase(strWord)) {
-                break;
-            } else if("старт".equalsIgnoreCase(strWord)){
-                while (true){
-
-                    byte word1[] = new byte[256];
-                    int counterChar1 = 0;
-                    try {
-                        counterChar1 = System.in.read(word1);
-                    } catch (Exception ex) {
-                        System.out.println(ex.toString());
-                        break;
-                    }
-                    String strWord1 = new String(word1, 0, counterChar).trim();
-
-                }}}}}
-
-                else {
-                    String[] bukvy;
-                    Stoyka a = new Stoyka();
-                    int miss = 0;
-                    Slovo b = new Slovo();
-                    String worldShadow = b.randomWorld();
-                    String str = worldShadow;
-                    char[] chars = new char[str.length()];
-                    for (int i = 1; i < (str.length() - 1); i++) {
-                        chars[i] = str.charAt(i);
-                    }
-                    bukvy = new String[6];
-                    for (int i = 1; i <= 6; i++) {
-                        bukvy[i - 1] = String.valueOf(chars[i]);
-                    }
-                    String[] quests = {"_ ", "_ ", "_ ", "_ ", "_ ", "_ "};
-                    a.stoyka(miss);}
-                for (int i = 0; i < 6; i++) {
-
-                    if (strWord.equals(bukvy[i])) {
-                        quests[i] = bukvy[i];
-                        break;
-                    } else if (i == 5) {
-                        miss = miss + 1;
-                        System.out.println("Ошибка " + miss);
-                        break;
-
-                    }
-                    for (int j = 0; j < 6; j++) {
-                        System.out.print(quests[j]);
-                    }
-
-                }
-            }
-        }
-    }
-}
-*/
--- src/Game.java
+++ src/Game.java
@@ -11,88 +11,55 @@ public class Game {
         stand.stand(errorNumber);                        // рисую стойку
         WordForARiddle hiddenWord = new WordForARiddle();
         String nextSecretWorld = hiddenWord.WordForARiddle();  // загадываю слово
-        char[] spellTheWorld = new char[nextSecretWorld.length()];   // создаю массив с буквами
+        String[] spellTheWorld = new String[nextSecretWorld.length()];   // создаю массив с буквами
         for (int i = 0; i < nextSecretWorld.length(); i++) {
-            spellTheWorld[i] = nextSecretWorld.charAt(i);
+            spellTheWorld[i] = String.valueOf(nextSecretWorld.charAt(i));
         }
-        System.out.println(Arrays.toString(spellTheWorld) + " chars ");
+        //  System.out.println(Arrays.toString(spellTheWorld) + " chars "); строка проверки загаданного слова
         String[] worldMask = new String[nextSecretWorld.length()];
         for (int i = 0; i < nextSecretWorld.length(); i++) {
             worldMask[i] = "* ";
         }
         System.out.println(Arrays.toString(worldMask));  // рисую маску слова
-        while (true) {
-
+        System.out.println("Попробуй отгадать букву");
+        while (errorNumber < 6) {
             Scanner scanner = new Scanner(System.in);
-            String playerInput = scanner.next();
+            String playerInput = scanner.next().toLowerCase();
+
             String regex = "[а-яёА-ЯЁ]+";
             Pattern pattern = Pattern.compile(regex);
             Matcher m = pattern.matcher(playerInput);
             if (playerInput.length() == 1) {         // проверяю длинну ввода
                 if (m.find()) {                     // проверяю на кириллицу
 
-                    System.out.println("Символ " + playerInput + " подходит");
-                } else
+                    boolean isCorrectGuess = false;
+                     for (int i = 0; i < spellTheWorld.length; i++) {
+                        if (spellTheWorld[i].equals(playerInput)) {
+                            worldMask[i] = playerInput;
+                            isCorrectGuess = true;
+                        }}
+                     if (isCorrectGuess) {
+                         System.out.println("Угодал");
+                         System.out.println(Arrays.toString(worldMask));
+                         if(Arrays.equals(worldMask, spellTheWorld)){
+                             break;
+                         }
+                     } else {
+                         errorNumber += 1;
+                         stand.stand(errorNumber);
+                     }
+
+
+                } else {
                     System.out.println("Символ " + playerInput + " не подходит");
-            }else {
+                    System.out.println("Символ должен содержать одну кириллическую букву");
+                }
+            } else {
                 System.out.println("Надо ввести одну букву " + playerInput + " не подходит");
+
             }
         }
     }
 }
-
-
-
-
-
-
-           /*
-
-            byte letter[] = new byte[256];
-            int counterChar = 0;
-            try {
-                counterChar = System.in.read(letter);
-            } catch (Exception ex) {
-                System.out.println(ex.toString());
-                break;
-            }
-            System.out.println(counterChar + " counterChar");
-
-            String strWord = new String(letter, 0, counterChar).trim();
-            System.out.println(strWord + " strWord");
-
-
-            if ("выход".equalsIgnoreCase(strWord)) {
-                break;
-
-
-            }  else {
-                if (strWord.equals(spellTheWorld[0])) {
-                    worldMask[0] = spellTheWorld[0];
-                } else if (strWord.equals(spellTheWorld[1])) {
-                    worldMask[1] = spellTheWorld[1];
-                } else if (strWord.equals(spellTheWorld[2])) {
-                    worldMask[2] = spellTheWorld[2];
-                } else if (strWord.equals(spellTheWorld[3])) {
-                    worldMask[3] = spellTheWorld[3];
-                } else if (strWord.equals(spellTheWorld[4])) {
-                    worldMask[4] = spellTheWorld[4];
-                } else if (strWord.equals(spellTheWorld[5])) {
-                    worldMask[5] = spellTheWorld[5];
-                }  else  {
-
-                    errorNumber = errorNumber + 1;
-                    System.out.println("Ошибка " + errorNumber);
-                    stand.stand(errorNumber);
-                }
-
-                System.out.print("\033[H\033[J");
-                stand.stand(errorNumber);
-                for (int j = 0; j < 6; j++) {
-                    if (j == 5) {
-                        System.out.println(" " + worldMask[j]);
-                    } else {
-                        System.out.print(" " + worldMask[j]);
-                    }*/
 
 
